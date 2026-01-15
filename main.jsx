70ca80887a29b5e9528325d9498ea420bd4a9743
patnamdoctor-maker <patnamdoctor@gmail.com>
2026-01-15 19:33:29 +0530

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+
+/**
+ * We have removed the import for './index.css' because 
+ * styling is handled by the Tailwind CDN in your index.html.
+ */
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
